aa8c2b1a2b498ffc55c3f5b1a72044a1d048914b
GDTran15 <trangiaiduong1512@gmail.com>
2025-10-13 09:51:25 +1100

    DTO for register request

---
+++ backend/WebApplication1/DTO/User/RegisterRequestDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTO.User
+{
+    public class RegisterRequestDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(5, ErrorMessage = "Username must be at least 5 characters")]
+        public string Username { get; set; }
+
+        [Required]
+        [MinLength(4, ErrorMessage = "Pasword must be at least 5 characters")]
+        public string Password { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+    }
+}
