ef94e38262e4ef68702a9cf4577482b5aa47d6ba
GDTran15 <trangiaiduong1512@gmail.com>
2025-10-12 20:57:10 +1100

    repository setup

--- backend/WebApplication1/IRepositories/IUserRepository.cs
+++ backend/WebApplication1/IRepositories/IUserRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace WebApplication1.IRepositories
+﻿using WebApplication1.Model;
+
+namespace WebApplication1.IRepositories
 {
     public interface IUserRepository
     {
+        Task<List<User>> GetAll();
+        Task<User?> GetByIdAsync(int id);
+        Task<long> CreateAsync(User user);
+        Task<bool> UpdateAsync(User user);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> IsExistByIdAsync(int id);
     }
 }
--- backend/WebApplication1/Repositories/UserRepository.cs
+++ backend/WebApplication1/Repositories/UserRepository.cs
@@ -1,6 +1,54 @@
-﻿namespace WebApplication1.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+using WebApplication1.IRepositories;
+using WebApplication1.Model;
+
+namespace WebApplication1.Repositories
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        #region Constructor and DI
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        #endregion
+
+        public async Task<long> CreateAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user.UserId;
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<User>> GetAll()
+        {
+            var list = await _context.Users.ToListAsync();
+            return list;
+        }
+
+        public async Task<User?> GetByIdAsync(int id)
+        {
+            var user = await _context.Users.Where(u => u.UserId == id).FirstOrDefaultAsync();
+            return user;
+        }
+
+        public async Task<bool> IsExistByIdAsync(int id)
+        {
+            var student = await GetByIdAsync(id);
+            return student is not null;
+        }
+
+        public async Task<bool> UpdateAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
