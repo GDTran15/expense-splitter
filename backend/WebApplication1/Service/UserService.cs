a52318be397a9015309ad10ecdfa9f5abda6a89d
GDTran15 <trangiaiduong1512@gmail.com>
2025-10-13 09:53:06 +1100

    User service

---
+++ backend/WebApplication1/Service/UserService.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using WebApplication1.DTO.User;
+using WebApplication1.IRepositories;
+using WebApplication1.Model;
+
+namespace WebApplication1.Service
+{
+    public class UserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        internal async Task CreateNewUser(RegisterRequestDTO requestDTO)
+        {
+            var userExist = await _userRepository.IsExistByUsernameOrEmailAsync(requestDTO.Username,requestDTO.Email);
+            if (userExist)
+            {
+                throw new InvalidOperationException("Username or email has been used");
+            }
+            var newUser = new User
+            {
+                Name = requestDTO.Name,
+                Username = requestDTO.Username,
+                Email = requestDTO.Email,
+                Password = requestDTO.Password,
+                Phone = requestDTO.Phone,
+                AmountSpend = 0
+
+            };
+            await _userRepository.CreateUserAsync(newUser);
+        }
+    }
+}
