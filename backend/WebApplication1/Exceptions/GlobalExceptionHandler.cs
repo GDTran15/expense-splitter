1ec7b38e8f67f47a99ad1b4e0993e7b81cb936ea
GDTran15 <trangiaiduong1512@gmail.com>
2025-10-13 09:52:00 +1100

    Global Exception setup

---
+++ backend/WebApplication1/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace WebApplication1.Exceptions
+{
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+        {
+
+            if (exception is InvalidOperationException)
+            {
+                httpContext.Response.StatusCode = StatusCodes.Status409Conflict;
+                await httpContext.Response.WriteAsJsonAsync(exception.Message, cancellationToken);
+               
+                return true;
+            }
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await httpContext.Response.WriteAsJsonAsync("An unexpected error occurred.", cancellationToken);
+
+            return true;
+        }
+    }
+}
