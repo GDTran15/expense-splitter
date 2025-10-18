e63f84be7025f757a2cc67515c9f54e005ac6452
Braeden <braeden.correa@student.uts.edu.au>
2025-10-18 13:12:29 +1100

    begin

--- frontend/src/Page/ExpensePage.jsx
+++ frontend/src/Page/ExpensePage.jsx
@@ -1,4 +1,30 @@
+import axios from "axios";
+import {  useEffect, useState } from "react"
+import { Container,Row,Col,Button,Modal,Form } from "react-bootstrap"
+
 export default function ExpensePage(){
+    const [show,setShow] = useState(false);
+    const [amount, setAmount] = useState("");
+    const [error,setError] = useState("");
+    const [expenseDate, setExpenseDate] = useState(new Date().toISOString().slice(0, 10)); //double check got from internet
+
+    const handleCreateExpense = async (e) => {
+        e.preventDefault();
+        setError("");
+        try {
+            const res = await axios.post("http://localhost:5165/expense", {
+                expenseAmount: amount,
+                expenseDate: new Date(expenseDate).toISOString(), //double check got from internet
+                userId: user.userId
+            });
+            console.log(res);
+        } catch(error) {
+            setError(error.response.data)
+        }
+    }
+    
+    
+    //build the page you get me 
     return(
         <>
         <h1>Expense</h1>
